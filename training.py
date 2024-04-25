a40d1a06e913da7373e6720f6de5651ebfb317c7
Doni <mhd0712@gmail.com>
2024-04-25 15:07:50 +0200

    implement k workers

--- training.py
+++ training.py
@@ -39,3 +39,40 @@ def train(agent, actor_optimizer, critic_optimizer, experience):
 
     return actor_loss.item(), critic_loss.item()
 
+def train_k(agent, actor_optimizer, critic_optimizer, experiences):
+
+    actor_loss = torch.tensor(0.0, requires_grad=True)
+    critic_loss = torch.tensor(0.0, requires_grad=True)
+    
+    for experience in experiences:
+        state, action, reward, next_state, terminated = experience
+        reward = torch.tensor(reward, dtype=torch.float).to(agent.device)
+        terminated = torch.tensor(terminated, dtype=torch.float).to(agent.device)
+    
+        # Get the V values from critic network
+        current_V_value = agent.critic(state)
+        next_V_value = agent.critic(next_state)
+    
+        # Get the log policy for taken action from actor
+        logits = agent.actor.forward(state)
+        probs = Categorical(logits=logits)
+        log_prob = probs.log_prob(action)
+    
+        # Compute the advantage
+        advantage = reward + (1-terminated)*agent.gamma*next_V_value - current_V_value
+        critic_loss += advantage.pow(2).mean()
+        actor_loss += -(log_prob * advantage.detach())
+        
+
+    # Gradient descent for the critic    
+    critic_optimizer.zero_grad()
+    critic_loss.backward()
+    critic_optimizer.step()
+
+    # Gradient descent for the actor
+    actor_optimizer.zero_grad()
+    actor_loss.backward()
+    actor_optimizer.step()
+
+
+    return actor_loss.item(), critic_loss.item()
