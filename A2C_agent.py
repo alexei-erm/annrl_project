4c4d5a0e76db43ac92ec8ee93efadd24c9dfd54f
Seba Jun <alexei.ermochkine@gmail.com>
2024-04-26 10:52:32 +0200

    added stuff for nstep batch agent3

--- A2C_agent.py
+++ A2C_agent.py
@@ -9,13 +9,15 @@ from models import Actor_network, Critic_network
 
 
 class Agent:
-    def __init__(self, input_size, hidden_size=64, \
+    def __init__(self, k, n, input_size, hidden_size=64, \
                 output_size_actor=2, output_size_critic=1, \
                 gamma=0.99, lr_actor=1e-5, lr_critic=1e-3, device="cpu"):
         super().__init__()
         self.actor = Actor_network(input_size, hidden_size, output_size_actor, device)
         self.critic = Critic_network(input_size, hidden_size, output_size_critic, device)
         
+        self.k = k
+        self.n = n
         self.device = device
         self.gamma = gamma
         self.lr_actor = lr_actor
@@ -35,21 +37,6 @@ class Agent:
             action = torch.argmax(probs.probs)
         return action
 
-
-    def save(self, path):
-        """
-        save the agent's models
-        """
-        torch.save(self.actor, path + "actor.pth")
-        torch.save(self.critic, path + "critic.pth")
-
-    def load(self, path):
-        """
-        load the agent's models
-        """
-        self.actor = torch.load(path + "actor.pth")
-        self.critic = torch.load(path + "critic.pth")
-
     def evaluate_agent(self, num_episodes=10):
         """
         evaluate the agent's performance
@@ -68,7 +55,7 @@ class Agent:
             while not done:
                 action = self.select_action(state, mode="evaluation")
                 value = self.critic(state)
-                value_trajectory.append(value.item())
+                value_trajectory.append(value)
                 next_state, reward, terminated, truncated, _ = test_env.step(action.item())
                 next_state = torch.from_numpy(next_state).float().to(self.device)  # Convert next_state to a tensor
                 episode_reward += reward
